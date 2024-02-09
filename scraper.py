f6c31334f35053aa4505dfe9add52ee4f62bf478
andrew <abartlin@uci.edu>
2024-02-09 15:14:24 -0800

    fixed scraper

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from urllib.parse import urlparse,urldefrag,urljoin
 from bs4 import BeautifulSoup
 import tokenizer
 from simhash import Simhash, SimhashIndex
+import requests
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -71,8 +72,8 @@ def is_valid(url):
             return False
 
 
-
-        if not re.match(
+        
+        return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|ogg|ogv|pdf"
@@ -81,10 +82,6 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
-        elif  False==domains_match(parsed.netloc):
-            print("failed:",url)
-            return False
-        print("passed: ", url)
     except TypeError:
         print ("TypeError for ", parsed)
         raise
