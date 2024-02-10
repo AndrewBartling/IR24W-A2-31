71e02add88310c89d39cc936a1a3fdc9ef49712a
Andrew James Bartling <abartlin@circinus-46.ics.uci.edu>
2024-02-10 02:12:41 -0800

    extracted urls for webpage, saved scraped urls to text file

--- scraper.py
+++ scraper.py
@@ -1,8 +1,9 @@
 import re
-from urllib.parse import urlparse,urldefrag,urljoin
+from urllib.parse import urlparse, urljoin
 from bs4 import BeautifulSoup,SoupStrainer
 import tokenizer
 from simhash import Simhash, SimhashIndex
+import httplib2
 
 def scraper(url, resp):
     links = extract_next_links(url, resp)
@@ -18,43 +19,52 @@ def extract_next_links(url, resp):
     #         resp.raw_response.url: the url, again
     #         resp.raw_response.content: the content of the page!
     # Return a list with the hyperlinks (as strings) scrapped from resp.raw_response.content
-    url_pattern = r'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'
-    url = set()
-
     try:
 
         if resp.status == 400:
-            return resp.status
+            return resp.error
 
         elif resp.status == 300:
             #redirect
             pass
         elif resp.status == 200:
             #succcess token the webpage
-            #save all words tokenized 
-            #longest page in term of number of words 
-            print(resp.raw_response.content)
-            links = BeautifulSoup(resp.raw_response.content,parse_only=SoupStrainer('a'))
-            text = BeautifulSoup(resp.raw_response.content,'html.parser')
-            #now can tokenize the soup and grab any urls from it
 
+            largefile = False
+            content_length = resp.raw_response.headers.get('Content-Length')
+            if content_length and int(content_length) > 10 * 1024 * 1024:  
+                print("Skipping due to large content size:", resp.url)
+                largefile = True            
+            # Parse the HTML content using BeautifulSoup
+            soup = BeautifulSoup(resp.raw_response.content, 'html.parser')
+            
+            # Extract text content and count words
+            text = soup.get_text()
+            
 
-            size = resp.raw_response.content.headers.get('Content-Lenght')            
+            tokens,total_words =tokenizer.tokenize(text)
+            
+            min_word_count = 20 
+             
+            links = set()
+            for link in soup.find_all('a'):
+                href = link.get('href')
+                if href and href.startswith(('http://', 'https://')):
+                    links.add(href)
+                elif href:
+                    links.add(urljoin(url, href))
 
+            if total_words < 20 and largefile:
+                #if a page has less than 20 words you shouldn't save it 
+                return list()
+            with open("scrapered.txt","a")as file:
+                file.write(url +"\n")
 
-            print("size: ", size,"\n")
-
-
-
-
-            for i in links:
-                print(i," ",end="")
-                url.add(i)
-
+            return list(links)
         #include simhashing for simliarity
 
 
-
+        return list(url)
         #check for redirect?
     except Exception as e:
         print("ERROR: ",e)
@@ -79,7 +89,6 @@ def is_valid(url):
     # Decide whether to crawl this url or not. 
     # If you decide to crawl it, return True; otherwise return False.
     # There are already some conditions that return False.
-    print("is_valid")
     try:
         parsed = urlparse(url)
         if parsed.scheme not in set(["http", "https"]):
@@ -87,6 +96,8 @@ def is_valid(url):
         if False ==domains_match(parsed.netloc):
             print(url)
             return False
+
+        
         return not re.match(
             r".*\.(css|js|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
@@ -96,6 +107,8 @@ def is_valid(url):
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
             + r"|rm|smil|wmv|swf|wma|zip|rar|gz)$", parsed.path.lower())
+        
+        print("is_valid,", url)
     except TypeError:
-        print ("TypeError for ", parsed)
+        print ("TypeError for ", url)
         raise
